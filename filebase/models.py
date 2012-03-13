aecbae7a9bdf03f18abfc7677dfd435c713b6902
matthias.bock <matthias.bock@hu-berlin.de>
2012-03-13 12:26:50 +0000

    new models

--- filebase/models.py
+++ filebase/models.py
@@ -9,18 +9,24 @@
 
 from django.db import models
 
+class Exes(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
+    file = models.IntegerField(db_column='File') # Field name made lowercase.
+    icon = models.TextField(db_column='Icon') # Field name made lowercase.
+    class Meta:
+        db_table = u'EXEs'
+
 class Files(models.Model):
     id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
-    folder = models.IntegerField(db_column='Folder') # Field name made lowercase.
-    filename = models.CharField(max_length=1500, db_column='FileName') # Field name made lowercase.
-    created = models.DateTimeField(db_column='Created') # Field name made lowercase.
-    lastmodification = models.DateTimeField(db_column='LastModification') # Field name made lowercase.
-    filesize = models.BigIntegerField(db_column='FileSize') # Field name made lowercase.
+    folder = models.IntegerField(null=True, db_column='Folder', blank=True) # Field name made lowercase.
+    filename = models.CharField(max_length=1500, db_column='Filename') # Field name made lowercase.
+    created = models.DateTimeField(null=True, db_column='Created', blank=True) # Field name made lowercase.
+    modified = models.DateTimeField(null=True, db_column='Modified', blank=True) # Field name made lowercase.
+    filesize = models.BigIntegerField(db_column='Filesize') # Field name made lowercase.
     md5 = models.CharField(max_length=96, db_column='MD5', blank=True) # Field name made lowercase.
-    filetype = models.CharField(max_length=1500, db_column='FileType', blank=True) # Field name made lowercase.
+    filetype = models.CharField(max_length=1500, db_column='Filetype', blank=True) # Field name made lowercase.
+    mime = models.CharField(max_length=300, db_column='MIME', blank=True) # Field name made lowercase.
     thumbnail = models.TextField(db_column='Thumbnail', blank=True) # Field name made lowercase.
-    lastsync = models.DateTimeField(db_column='LastSync') # Field name made lowercase.
-    syncresult = models.CharField(max_length=30, db_column='SyncResult') # Field name made lowercase.
     class Meta:
         db_table = u'Files'
 
@@ -29,7 +35,14 @@ class Folders(models.Model):
     parent = models.IntegerField(db_column='Parent') # Field name made lowercase.
     name = models.CharField(max_length=1500, db_column='Name') # Field name made lowercase.
     created = models.DateTimeField(db_column='Created') # Field name made lowercase.
-    lastsync = models.DateTimeField(db_column='LastSync') # Field name made lowercase.
     class Meta:
         db_table = u'Folders'
+
+class Jpegs(models.Model):
+    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
+    file = models.IntegerField(db_column='File') # Field name made lowercase.
+    exif = models.TextField(db_column='EXIF', blank=True) # Field name made lowercase.
+    thumbnail = models.TextField(db_column='Thumbnail', blank=True) # Field name made lowercase.
+    class Meta:
+        db_table = u'JPEGs'
 
